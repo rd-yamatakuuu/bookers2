a2070bace938f9e789f383dc91dfaca47853c0eb
rd-yamatakuuu <orgchem.techac@gmail.com>
2021-04-13 11:10:00 +0000

    [Finish]miss

---
+++ bookers2/app/controllers/books_controller.rb
@@ -0,0 +1,20 @@
+class BooksController < ApplicationController
+  
+  def new
+  end 
+  
+  def create
+  end 
+  
+  def index
+    @books = Book.all
+    @book = Book.new
+  end 
+  
+  def show
+  end 
+  
+  def destroy
+  end 
+  
+end
--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+ 
+  get 'users/show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'books#top'
+  
+  resources :book, only: [:new, :create, :index, :show, :destroy]
+  
+  
 end

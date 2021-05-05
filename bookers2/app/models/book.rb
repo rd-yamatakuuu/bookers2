ca416cf94b4ba10d81b599d1656c7e4b318072bc
rd-yamatakuuu <orgchem.techac@gmail.com>
2021-05-05 13:20:29 +0000

    [fix]finished-show-ajax

--- bookers2/app/models/book.rb
+++ bookers2/app/models/book.rb
@@ -4,7 +4,7 @@ class Book < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   
   def favorited_by?(user)
-    favorites.where(user_id: user.id).exists?
+    Favorite.where(user_id: user.id).exists?
   end 
 
   validates :title, presence: true

d0625e24dfdbaf2ab56f0d1a30dbbb82b7fff837
rd-yamatakuuu <orgchem.techac@gmail.com>
2021-05-06 11:49:50 +0000

    [finish]search-prefecture

--- bookers2/db/schema.rb
+++ bookers2/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_04_014750) do
+ActiveRecord::Schema.define(version: 2021_05_06_100437) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -61,6 +61,11 @@ ActiveRecord::Schema.define(version: 2021_05_04_014750) do
     t.text "introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "postcode"
+    t.integer "prefecture_code"
+    t.string "address_city"
+    t.string "address_street"
+    t.string "address_building"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

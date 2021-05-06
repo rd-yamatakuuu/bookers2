428cfd85712f6db7bec7457c10276bd93ced9c0b
rd-yamatakuuu <orgchem.techac@gmail.com>
2021-05-06 07:10:07 +0000

    [finish]ajax

--- bookers2/app/controllers/book_comments_controller.rb
+++ bookers2/app/controllers/book_comments_controller.rb
@@ -1,23 +1,25 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = BookComment.new(book_comment_params)
-    comment.user_id = current_user.id
-    comment.book_id = book.id
-    if comment.save
-      redirect_to book_path(book)
-    else
-      @book = Book.new
-      @user = @book.user
-      @books = Book.all
-      render 'books/index'
-    end
+    @book = Book.find(params[:book_id])
+    #book = Book.find(params[:book_id])
+    @book_comment = BookComment.new(book_comment_params)
+    @book_comment.user_id = current_user.id
+    @book_comment.book_id = @book.id
+    @book_comment.save
+      #redirect_to book_path(book)
+    #else
+      #@book = Book.new
+      #@user = @book.user
+      #@books = Book.all
+      #render 'books/index'
+    #end
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+  	@book_comment = @book.book_comments.find(params[:id])
+		@book_comment.destroy
 
   end
 
